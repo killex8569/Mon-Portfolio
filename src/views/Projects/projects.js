961d1bbc7deaa24d94927069645373e238b7c6b9
killex8569 <alexandreanette@gmail.com>
2026-04-25 15:50:57 +0200

    Feat: fix UseNavigate error vercel

--- src/views/Projects/projects.js
+++ src/views/Projects/projects.js
@@ -1,6 +1,6 @@
 // Projets.jsx
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './projects.css';
 
 function getAllTechs(arr) {
